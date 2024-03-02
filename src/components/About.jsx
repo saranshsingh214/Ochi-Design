cfc8958f987e0ad8f8a17f3cd461c8b270a97564
Saransh Singh <saranshsingh.it2021@nsec.ac.in>
2024-03-03 02:37:28 +0530

    Marquee Done

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='w-full py-20 bg-[#CDEA68] rounded-t-3xl'>
+      <h1 className='font-["Neue_Montreal"] text-5xl px-[8vw] text-zinc-900'>Ochi is a strategic partner for fast-grow­ing tech businesses that need to raise funds, sell prod­ucts, ex­plain com­plex ideas, and hire great people.</h1>
+    </div>
+  )
+}
+
+export default About
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { BsArrowUpRightCircle, BsFillLightbulbFill, BsLightbulb } from "react-icons/bs";
+
 
 function LandingPage() {
   return (
@@ -6,19 +8,24 @@ function LandingPage() {
         <div className="textStructure mt-36 px-20">
             {["We Create","Eye-Opening","Presentations"].map((item,index)=>{
                 return <div key={index} className="masker">
-                            <h1 className="uppercase text-8xl leading-[7vw] font-semibold font-['Test_Founders_Grotesk']">
-                                {item}
-                            </h1>
+                            <div className="flex items-end  w-fit">
+                            {index ===1 && (<div className=' mr-[1vw] w-[8vw] rounded-md bg-red-900 h-[5.7vw] relative -top-[1.2vw]'></div>)}
+                                <h1 className="uppercase h-full text-[9vw] -mb-[1vw] pt-[2vw] leading-[.75]  font-medium font-['Founders_Grotesk_X']">
+                                    {item}
+                                </h1>
+                            </div>
                         </div>
             })}
             
         </div>
-        <div className='border-t-[1px] mt-28 border-zinc-700 flex justify-between py-4 px-20'>
+        <div className='border-t-[1px] mt-20 border-zinc-700 flex justify-between py-4 px-20'>
             {["For public and private companies","From the first pitch to IPO"].map((item,index) =>(
                 <p className='text-md font-light font-["Neue Montreal"]'>{item}</p>
             ))}
-            <div className='font-["Test_Founders_Grotesk"]'>
-                START THE PROJECT
+            <div className='font-["Test_Founders_Grotesk"] flex items-center justify-center gap-1'>
+                <div className="px-4 py-1 border-[2px] font-light text-md rounded-full cursor-pointer">START THE PROJECT</div>
+                <div className="arrow"><BsArrowUpRightCircle size={30}/>
+</div>
             </div>
         </div>
     </div>
